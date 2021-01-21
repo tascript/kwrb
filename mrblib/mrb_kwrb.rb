891d3d9456a6e3a5da61e17b7459e9aa4ce69144
wataru-script <wataruscript8611@gmail.com>
2021-01-22 02:26:14 +0900

    prepare method base

--- mrblib/mrb_kwrb.rb
+++ mrblib/mrb_kwrb.rb
@@ -1,2 +1,18 @@
 class Kwrb < TCPSocket
+
+  def initialize(topic)
+    @topic = topic.to_s
+  end
+
+  def connect
+  end
+
+  def publish
+  end
+
+  def subscribe
+  end
+
+  def close
+  end
 end
