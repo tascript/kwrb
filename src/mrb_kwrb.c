2cb5027aa7919c0c85a5c84a3142d88b29e03a87
tascript <wataruscript8611@gmail.com>
2021-03-23 21:53:28 +0900

    initialize

--- src/mrb_kwrb.c
+++ src/mrb_kwrb.c
@@ -12,52 +12,6 @@
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-typedef struct
-{
-  char *str;
-  mrb_int len;
-} mrb_kwrb_data;
-
-static const struct mrb_data_type mrb_kwrb_data_type = {
-    "mrb_kwrb_data",
-    mrb_free,
-};
-
-static mrb_value mrb_kwrb_init(mrb_state *mrb, mrb_value self)
-{
-  mrb_kwrb_data *data;
-  char *str;
-  mrb_int len;
-
-  data = (mrb_kwrb_data *)DATA_PTR(self);
-  if (data)
-  {
-    mrb_free(mrb, data);
-  }
-  DATA_TYPE(self) = &mrb_kwrb_data_type;
-  DATA_PTR(self) = NULL;
-
-  mrb_get_args(mrb, "s", &str, &len);
-  data = (mrb_kwrb_data *)mrb_malloc(mrb, sizeof(mrb_kwrb_data));
-  data->str = str;
-  data->len = len;
-  DATA_PTR(self) = data;
-
-  return self;
-}
-
-static mrb_value mrb_kwrb_hello(mrb_state *mrb, mrb_value self)
-{
-  mrb_kwrb_data *data = DATA_PTR(self);
-
-  return mrb_str_new(mrb, data->str, data->len);
-}
-
-static mrb_value mrb_kwrb_hi(mrb_state *mrb, mrb_value self)
-{
-  return mrb_str_new_cstr(mrb, "hi!!");
-}
-
 void mrb_kwrb_gem_init(mrb_state *mrb)
 {
   struct RClass *kwrb;
