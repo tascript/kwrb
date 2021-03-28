4cdf037d68dfd75560cdd09e1e509ec5c829b4e3
tascript <wataruscript8611@gmail.com>
2021-03-29 01:47:12 +0900

    update mrb_queue_generator

--- src/mrb_kwrb.c
+++ src/mrb_kwrb.c
@@ -9,22 +9,26 @@
 #include <mruby.h>
 #include <mruby/error.h>
 #include <mruby/string.h>
+#include <stdlib.h>
 #include "mrb_kwrb.h"
+
+#define LIMIT 100
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-static mrb_value mrb_queue_generator(mrb_state *mrb, mrb_value self)
-{
+static mrb_value mrb_queue_generator(mrb_state *mrb, mrb_value self) {
+  char *queue = (char *)malloc(LIMIT * sizeof(char));
+  for (i = 0; i < LIMIT; i++) {
+    queue[i] = '';
+  }
 }
 
-void mrb_kwrb_gem_init(mrb_state *mrb)
-{
+void mrb_kwrb_gem_init(mrb_state *mrb) {
   struct RClass *queue;
   queue = mrb_define_class(mrb, "Queue", mrb->object_class);
   mrb_define_method(mrb, queue, "initialize", mrb_queue_generator, MRB_ARGS_NONE());
   DONE;
 }
 
-void mrb_kwrb_gem_final(mrb_state *mrb)
-{
+void mrb_kwrb_gem_final(mrb_state *mrb) {
 }
