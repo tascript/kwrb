f306579b6da4b118329b2779ad034c3004389495
tascript <wataruscript8611@gmail.com>
2021-04-03 15:59:49 +0900

    crate queue base

--- src/mrb_kwrb.c
+++ src/mrb_kwrb.c
@@ -17,34 +17,64 @@
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-typedef struct {
+typedef struct
+{
   char data[LIMIT];
   int head;
   int tail;
 } kwrb_queue
 
-const static struct mrb_data_type mrb_queue_type = { "Queue", mrb_free }
+    const static struct mrb_data_type mrb_queue_type = {"Queue", mrb_free}
 
-static mrb_value mrb_queue_generator(mrb_state *mrb, mrb_value self) {
-  kwrb_queue *queue = malloc(sizeof(kwrb_queue));
+static mrb_value
+mrb_queue_generator(mrb_state * mrb, mrb_value self)
+{
+  kwrb_queue *queue = (struct kwrb_queue *)mrb_malloc(mrb, sizeof(kwrb_queue));
   queue->head = 0;
   queue->tail = 0;
-  for (i = 0; i < LIMIT; i++) {
+  for (i = 0; i < LIMIT; i++)
+  {
     queue->data[i] = '';
   }
+  DATA_TYPE(self) = &mrb_queue_type;
+  DATA_PTR(self) = queue;
   return self;
 }
 
-static mrb_value mrb_enqueue(mrb_state *mrb, mrb_value self) {
+static mrb_value mrb_enqueue(mrb_state *mrb, mrb_value self)
+{
   mrb_value message;
   mrb_get_args(mrb, "S", &message);
+  int i, j, size;
+  kwrb_queue *q;
+  q = DATA_PTR(self);
+  size = RSTRING_LEN(message);
+  for (i = 0; i < size; i++)
+  {
+    if (q->tail >= LIMIT)
+    {
+      q->tail = 0;
+    }
+    q->data[q->tail] = message[i];
+    q->tail++;
+  }
+
   return mrb_nil_value();
 }
 
-static mrb_value mrb_dequeue(mrb_state *mrb, mrb_value self) {
+static mrb_value mrb_dequeue(mrb_state *mrb, mrb_value self)
+{
+  q = DATA_PTR(self);
+  if (q->head == q->tail)
+  {
+    return mrb_nil_value();
+  }
+  q->head++;
+  mrb_str_new_cstr(mrb, q->data[q->tail]);
 }
 
-void mrb_kwrb_gem_init(mrb_state *mrb) {
+void mrb_kwrb_gem_init(mrb_state *mrb)
+{
   struct RClass *queue;
   queue = mrb_define_class(mrb, "Queue", mrb->object_class);
   MRB_SET_INSTANCE_TT(queue, MRB_TT_DATA);
@@ -54,5 +84,6 @@ void mrb_kwrb_gem_init(mrb_state *mrb) {
   DONE;
 }
 
-void mrb_kwrb_gem_final(mrb_state *mrb) {
+void mrb_kwrb_gem_final(mrb_state *mrb)
+{
 }
