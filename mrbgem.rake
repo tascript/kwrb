05ee566f284cd2a14cc8ff1ffa70ce77a956f6f8
tascript <wataruscript8611@gmail.com>
2021-04-03 19:52:53 +0900

    fix build configs

--- mrbgem.rake
+++ mrbgem.rake
@@ -1,6 +1,7 @@
 MRuby::Gem::Specification.new('kwrb') do |spec|
   spec.authors = 'Wataru Morita'
+  spec.license = 'MIT'
+  spec.summary = 'Kwrb is MQTT client for mruby'
 
-  spec.add_dependency = 'mruby-socket'
-  spec.add_dependency = 'mruby-thread'
+  spec.add_dependency('mruby-socket')
 end
--- src/mrb_kwrb.c
+++ src/mrb_kwrb.c
@@ -22,20 +22,16 @@ typedef struct
   char data[LIMIT];
   int head;
   int tail;
-} kwrb_queue
+} kwrb_queue;
 
-    const static struct mrb_data_type mrb_queue_type = {"Queue", mrb_free}
+const static struct mrb_data_type mrb_queue_type = {"Queue", mrb_free};
 
 static mrb_value
-mrb_queue_generator(mrb_state * mrb, mrb_value self)
+mrb_queue_generator(mrb_state *mrb, mrb_value self)
 {
-  kwrb_queue *queue = (struct kwrb_queue *)mrb_malloc(mrb, sizeof(kwrb_queue));
+  kwrb_queue *queue = (kwrb_queue *)mrb_malloc(mrb, sizeof(kwrb_queue));
   queue->head = 0;
   queue->tail = 0;
-  for (i = 0; i < LIMIT; i++)
-  {
-    queue->data[i] = '';
-  }
   DATA_TYPE(self) = &mrb_queue_type;
   DATA_PTR(self) = queue;
   return self;
@@ -64,6 +60,7 @@ static mrb_value mrb_enqueue(mrb_state *mrb, mrb_value self)
 
 static mrb_value mrb_dequeue(mrb_state *mrb, mrb_value self)
 {
+  kwrb_queue *q;
   q = DATA_PTR(self);
   if (q->head == q->tail)
   {
@@ -82,7 +79,7 @@ void mrb_kwrb_gem_init(mrb_state *mrb)
   struct RClass *queue;
   queue = mrb_define_class(mrb, "Queue", mrb->object_class);
   MRB_SET_INSTANCE_TT(queue, MRB_TT_DATA);
-  mrb_define_method(mrb, queue, "initialize", mrb_queue_generator, MRB_ARGS_NONE());
+  mrb_define_method(mrb, queue, "initialize", mrb_queue_init, MRB_ARGS_NONE());
   mrb_define_method(mrb, queue, "enqueue", mrb_enqueue, MRB_ARGS_REQ(1));
   mrb_define_method(mrb, queue, "dequeue", mrb_dequeue, MRB_ARGS_NONE());
   DONE;
