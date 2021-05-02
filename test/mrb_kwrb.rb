d47ded62f72dfbe6052092cba4c77a20bf6268f9
tascript <wataruscript8611@gmail.com>
2021-05-02 21:02:26 +0900

    add test for queue value

--- test/mrb_kwrb.rb
+++ test/mrb_kwrb.rb
@@ -3,3 +3,9 @@ assert('Queue#enqueue, Queue#dequeue') do
   q.enqueue('hello')
   assert_equal('hello', q.dequeue)
 end
+
+assert('Queue#value') do
+  q = Queue.new
+  q.enqueue('hello')
+  assert_equal(['hello'], q.value)
+end
