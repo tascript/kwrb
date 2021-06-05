d971b97523d3b6725def56985d67065017dde065
tascript <wataruscript8611@gmail.com>
2021-06-05 14:07:25 +0900

    read packet

--- mrblib/mrb_kwrb.rb
+++ mrblib/mrb_kwrb.rb
@@ -48,6 +48,15 @@ class Kwrb
 
             ping_packet = Kwrb::Packet::Pingreq.new
             s.syswrite ping_packet.data
+
+            begin
+              s.sysread MAXSIZE
+            rescue StandardError
+              nil
+            end
+
+            raise 'Failed: response is invalid' if res.nil?
+
             next
           end
 
